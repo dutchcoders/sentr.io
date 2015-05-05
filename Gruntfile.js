785f8e3812dd3f7427251b6a19df38d5b64a4b63
Dylan Wreggelsworth <dylan@bvrgroup.us>
2015-05-05 13:07:05 -0400

    jekyll setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,66 @@
+'use strict';
+
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    buildcontrol: {
+
+    jekyll: {
+      options: {
+        src: 'site'
+      },
+      dist: {
         options: {
-          dir: 'dist',
-          commit: true,
-          push: true,
-          message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
-        },
-        pages: {
-          options: {
-            remote: 'git@github.com/dutchcoders/sentr.io.git',
-            branch: 'gh-pages'
-          }
-        },
-        local: {
-          options: {
-            remote: '../',
-            branch: 'build'
-          }
+          dest: 'dist',
+        }
+      },
+      server: {
+        options: {
+          serve: true,
+          watch: true,
+          config: 'site/_config.yml',
+          dest: '.jekyll'
+        }
+      },
+      check: {
+        options: {
+          doctor: true
         }
       }
-  })
-  grunt.loadNpmTasks('grunt-build-control');
-}
+    },
+    buildcontrol: {
+      options: {
+        dir: 'dist',
+        commit: true,
+        push: true,
+        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+      },
+      pages: {
+        options: {
+          remote: 'git@github.com/dutchcoders/sentr.io.git',
+          branch: 'gh-pages'
+        }
+      },
+      local: {
+        options: {
+          remote: '../',
+          branch: 'build'
+        }
+      }
+    }
+  });
 
+  grunt.registerTask('default', ['jekyll:server']);
+  grunt.registerTask('build', [
+    'jekyll:dist'
+  ]);
+
+  grunt.registerTask('deploy', [
+    'build',
+    'buildcontrol'
+  ]);
+
+  grunt.loadNpmTasks('grunt-jekyll');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-build-control');
+};
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # sentry.io
 Sentry.io website
+
+## Setup
+```
+$ bundle install
+$ npm install -g grunt-cli
+```
+
+### Building
+`$ grunt build`
